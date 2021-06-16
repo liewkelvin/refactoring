b5980f79461b530f3291b8569b34fc09cc6d0bd5
kelvin.liew <kelvin.liew@SGSCLVA-55SDB63.YCH.COM>
2021-06-16 13:45:00 +0800

    to do list solution

---
+++ ToDoList Workshop Java/test/sg/edu/nus/iss/epat/tdd/workshop/ToDoListTest.java
@@ -0,0 +1,45 @@
+package sg.edu.nus.iss.epat.tdd.workshop;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class ToDoListTest extends TestCase {
+   // Define Test Fixtures
+
+   public ToDoListTest() {
+      super();
+   }
+
+   @Before
+   public void setUp() throws Exception {
+      // Initialise Test Fixtures
+   }
+
+   @After
+   public void tearDown() throws Exception {
+      // Uninitialise test Fixtures
+   }
+
+   @Test
+   public void testAddTask() {
+      fail("Not implemented yet");
+   }
+
+   @Test
+   public void testGetStatus() {
+      fail("Not implemented yet");
+   }
+
+   @Test
+   public void testRemoveTask() {
+      fail("Not implemented yet");
+   }
+
+   @Test
+   public void testGetCompletedTasks() {
+      fail("Not implemented yet");
+   }
+}
